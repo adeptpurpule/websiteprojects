ca05972becb2b2aba1c3494d247eb96db8570428
adeptpurpule <keegan_clarke@hotmail.com>
2018-01-11 14:51:29 +1000

    Revert "projects"
    This reverts commit 048a252c26fa39e1169adcd082b065d825c50816.

--- wordpress/wp-content/themes/bandtheme/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-?> 
-<div class="row">
-        <div class="twelve columns">
-          <ul id="menu3" class="footer_menu horizontal">
-            <li class=""><a href="index.html">Home</a></li>
-        </div>
-      </div>
